227c5e9da32f00890c9a45b147d245f73423ebd7
Lucas Demaimay <demaima231@campus.unicaen.fr>
2024-10-08 14:47:54 +0200

    affichage des villes selon un code postal

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-let para = document.createElement("p");
-const r = fetch("https://geo.api.gouv.fr/communes?codePostal=61570");
+let listeVille = document.getElementById("listeVille");
+listeVille
 
-r.then(response => {
-  return response.json()
-}).then(people => {
-  console.log(people);
-  para.textContent = people[0];
+let codePostal = document.getElementById("codePostal");
+
+codePostal.addEventListener("input", function() {
+  if(codePostal.value.length == 5){
+    afficherCommunes(codePostal.value);
+  }
+  
 });
+
+ function afficherCommunes(codePostal) {
+  const r = fetch("https://geo.api.gouv.fr/communes?codePostal="+codePostal);
+  r.then(response => {
+    return response.json()
+  }).then(villes => {
+  
+    listeVille.innerHTML += "<option value = \"\" >-- Choisissez une commune --</option>"
+    for(i = 0; i < villes.length; i++){
+      listeVille.innerHTML += "<option value = "+ villes[i]["nom"] +">"+ villes[i]["nom"] +"</option>"
+    }
+  
+    
+  });
+}
+
+
