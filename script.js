1344cd4e76fe227aeadb4dbd6102f2c41e026ab0
lucas_demaimay <demaimay.lucas@gmail.com>
2024-10-17 14:53:59 +0200

    amelioration du fetch des communes

--- script.js
+++ script.js
@@ -26,22 +26,29 @@ codePostal.addEventListener("input", function() {
   
 });
 
+async function fetchCommunes(codePostal) {
+  try {
+    const reponse = await fetch("https://geo.api.gouv.fr/communes?codePostal="+codePostal);
+    const data = await reponse.json();
+    return data;
+  } catch(error) {
+    console.error("Erreur lors des requêtes : ", error);
+    throw error;
+  }
+}
+
  async function afficherCommunes(codePostal) {
-  const r = fetch("https://geo.api.gouv.fr/communes?codePostal="+codePostal);
-  r.then(response => {
-     return response.json()
-  }).then(villes => {
-    
-    if(villes.length == 0){
-      listeVille.innerHTML += "<option value = \"\" >-- Aucune commune trouvée --</option>"
-      listeVille.style.appearance = "none";
-    } else {
-      listeVille.innerHTML += "<option value = \"\" >-- Choisissez une commune --</option>"
-      for(i = 0; i < villes.length; i++){
-        listeVille.innerHTML += "<option value = "+ villes[i]["code"] +">"+ villes[i]["nom"] +"</option>"
-      }
+  villes = await fetchCommunes(codePostal);
+  console.log(villes);
+  if(villes.length == 0){
+    listeVille.innerHTML += "<option value = \"\" >-- Aucune commune trouvée --</option>"
+    listeVille.style.appearance = "none";
+  } else {
+    listeVille.innerHTML += "<option value = \"\" >-- Choisissez une commune --</option>"
+    for(i = 0; i < villes.length; i++){
+      listeVille.innerHTML += "<option value = "+ villes[i]["code"] +">"+ villes[i]["nom"] +"</option>"
     }
-  });
+  }
 }
 
 
