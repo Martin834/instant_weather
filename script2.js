4cf1ed17f5eb853b20e9b1ec8a17601f18d8257c
Lucas Demaimay <demaima231@campus.unicaen.fr>
2024-10-18 10:37:32 +0200

    bug des infos non affichees fixe

--- script2.js
+++ script2.js
@@ -71,7 +71,6 @@ async function remplirElements(codeInsee){
         conteneurInfos.children.item(pos).querySelector("#Tmax").textContent = data["forecast"]["tmax"] + "°C";
         conteneurInfos.children.item(pos).querySelector("#probaPluie").textContent = data["forecast"]["probarain"] + "%";
         conteneurInfos.children.item(pos).querySelector("#nbHSoleil").textContent = data["forecast"]["sun_hours"] + "H";
-        console.log(conteneurInfos.children.item(pos).querySelector("#Latitude"));
         for (i = 0; i < optionsTrue.length; i++) {
             switch (optionsTrue[i]) {
                 case "latitude": conteneurInfos.children.item(pos).querySelector("#Latitude").textContent = data["forecast"]["latitude"]; mettreEnDisplayBlock(conteneurInfos.children.item(pos).querySelector("#ulLatitude")); break;
@@ -146,8 +145,8 @@ function mettreEnDisplayBlock(e) {
 // Récupération de l'URL
 let urlcourante = document.location.href;
 urlcourante = urlcourante.split(/[?=&]/);
-
-while (urlcourante[cpt] != "codeInsee") { // Indique les options choisis par l'utilisateur
+console.log(urlcourante)
+while (cpt < urlcourante.length) { // Indique les options choisis par l'utilisateur
     switch (urlcourante[cpt]) {
         case "latitude": optionsTrue.push("latitude"); break;
         case "longitude": optionsTrue.push("longitude"); break;
