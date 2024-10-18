4bfe3418aa284532aedaab0d881300ac21efa79c
Lucas Demaimay <demaima231@campus.unicaen.fr>
2024-10-18 09:34:51 +0200

    commentaire script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let listeVille = document.getElementById("listeVille");
+let listeVille = document.getElementById("listeVille"); // Liste déroulante contenant les communes
 listeVille.style.display = "none";
-let codePostal = document.getElementById("codePostal");
+let codePostal = document.getElementById("codePostal"); // Entrée textuel pour le code postal
 console.log(listeVille.style.appearance)
 
-codePostal.addEventListener("input", function() {
+codePostal.addEventListener("input", function() { //Quand on entre un chiffre dans codePostal
   
+  //Vérifie que le texte entré ne comporte que des chiffres
   code = "";
 
   for(char in codePostal.value){
@@ -15,17 +16,23 @@ codePostal.addEventListener("input", function() {
 
   codePostal.value = code;
 
-  if(codePostal.value.length == 5){
+  if(codePostal.value.length == 5){ // Si le code postal est complet on cherche la liste des communes correspondant
     afficherCommunes(codePostal.value);
     listeVille.style.display = "block";
     listeVille.style.appearance = "";
-  } else {
+  } else { // Si le code Postal est incomplet on cache la liste des communes
     listeVille.innerHTML = ""
     listeVille.style.display = "none";
   }
   
 });
 
+/**
+ * Permet de récupérer une liste de communes suivant un code postal
+ * 
+ * @param {*} codePostal 
+ * @returns une liste de communes
+ */
 async function fetchCommunes(codePostal) {
   try {
     const reponse = await fetch("https://geo.api.gouv.fr/communes?codePostal="+codePostal);
@@ -37,13 +44,18 @@ async function fetchCommunes(codePostal) {
   }
 }
 
- async function afficherCommunes(codePostal) {
+/**
+ * Remplit la liste déroulante avec les différentes communes
+ * 
+ * @param {*} codePostal 
+ */
+async function afficherCommunes(codePostal) {
   villes = await fetchCommunes(codePostal);
   console.log(villes);
-  if(villes.length == 0){
+  if(villes.length == 0){ // Si on a trouvé des villes
     listeVille.innerHTML += "<option value = \"\" >-- Aucune commune trouvée --</option>"
     listeVille.style.appearance = "none";
-  } else {
+  } else { // Si aucune ville n'a été trouvée
     listeVille.innerHTML += "<option value = \"\" >-- Choisissez une commune --</option>"
     for(i = 0; i < villes.length; i++){
       listeVille.innerHTML += "<option value = "+ villes[i]["code"] +">"+ villes[i]["nom"] +"</option>"
